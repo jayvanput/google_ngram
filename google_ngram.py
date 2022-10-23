745a9e94ae9ddd0df7262e4977ddf65c9afa9623
jvp119 <jayvanput@gmail.com>
2022-10-22 23:21:11 -0400

    Implemented basic querying with mutliple subjects

--- google_ngram.py
+++ google_ngram.py
@@ -1,29 +1,59 @@
 import requests
 import typing
+from dataclasses import dataclass
 
+class Params:
+    content: str
+    
 class GoogleNgram:
-
+    """Google Ngram object for storing parameters and JSON output."""
     def __init__(self) -> None:
-        self.url = ""
+        self._url = "https://books.google.com/ngrams/json?"
         self.data: typing.Dict[str, typing.Any] = {}
-        self.params: typing.Dict[str, str] = {}
+        self.params: typing.Dict[str, typing.Any] = {}
+
+    def set_param(self, param: str, value: str):
+        if param == "content" and self.params.get("content",False):
+            self.params[param] += f",{value}"
+        else:
+            self.params[param] = value
+
+    def update_url(self) -> None:
+        """Update the url with the latest parameters."""
+
+        url = "https://books.google.com/ngrams/json?"
+
+        for key, value in self.params.items():
+            if url[-1] == "?":
+                url = f"{url}{key}={value}"
+            else:
+                url = f"{url}&{key}={value}"
+        self._url = url
 
     def _set_data(self, data: typing.Dict[str, typing.Any]) -> None:
         self.data = data
 
-    def get_data(self) -> typing.Dict[str, typing.Any]:
+    def request_data(self) -> typing.Dict[str, typing.Any]:
+        """Call data from the built URL. Raises exception if the request doesn't return any data."""
+        self.update_url()
+        
+        response = requests.get(self._url)
+        
+        data = {}
+        for ngram in response.json():
+            word = ngram["ngram"]
+            time_series = ngram["timeseries"]
+
+            data[word] = time_series
+
+        self._set_data(data)
+
+        if not self.data:
+            self._set_data({})
+            raise Exception("The URL did not return any data.")
         return self.data
-
-    def request_data(self, url: str) -> typing.Dict[str, typing.Any]:
-        response = requests.get(url)
-        self._set_data(response.json()[0])
-
-        return self.data
-
-
-    def set_params(self):
-        pass
 
 if __name__ == "__main__":
     test = GoogleNgram()
-    print(type(test.get_data()), test.get_data())
+    test.set_param("content","Albert+Einstein")
+    print(test._url, type(test.request_data()), test.request_data())
--- test/test.py
+++ test/test.py
@@ -6,11 +6,29 @@ class TestGoogleNgram(unittest.TestCase):
 
     def test_request_returns_dict(self):
         ngram = google_ngram.GoogleNgram()
-        ngram.request_data("https://books.google.com/ngrams/json?content=Albert+Einstein")
+        ngram.set_param("content", "Albert+Einstein")
+        
+        ngram.request_data()
 
         self.assertIsInstance(ngram.data, dict)
 
-    
+    def test_bad_url_raises_error(self):
+        ngram = google_ngram.GoogleNgram()
+        self.assertRaises(Exception, ngram.request_data)
 
+    def test_content_isnt_give(self):
+        ngram = google_ngram.GoogleNgram()
+        ngram.set_param("start_year", "2000")
+        
+        self.assertRaises(Exception, ngram.request_data)
+
+    def test_multiple_contents_returns_each(self):
+        ngram = google_ngram.GoogleNgram()
+        ngram.set_param("content", "Albert+Einstein")
+        ngram.set_param("content", "Frankenstein")
+        
+        ngram.request_data()
+
+        self.assertEqual(len(ngram.data), 2)
 if __name__ == "__main__":
     unittest.main()
